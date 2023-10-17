e29753d57970704e9b455db0a27f7a759fc2a034
Victor Montelongo <victoramontelongo@gmail.com>
2023-10-16 20:49:00 -0500

    seems like local storage is working

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,6 @@
 const timeDisplay = $("#time-display");
 const bodyText = $(".text");
-var todos = []
+
 // handle displaying the time
 function displayTime() {
   currentTime = setInterval(function () {
@@ -12,44 +12,35 @@ displayTime();
 // attempting to save the text from the body in local storage
 
 // this should hold and create the blocks I need to hold the information
-const hourData = ""
+var hourData = []
 const currentState = dayjs().format('H');
 const hourBlock = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
-const btnSave = ".saveBtn"
+
 for (var i = 0; i < 9; i++) {
   // This should determine past, current, and future time with color
-  $("#row-display").append(` <div id=${i} class="row time-block ${currentState}">
+  $("#row-display").append(`<div id=${i} class="row time-block ${currentState}">
   <div class="col-2 col-md-1 hour text-center py-3">${hourBlock[i]}</div>
   <textarea id='text-09' class="col-8 col-md-10 description" rows="3"> ${hourData} </textarea>
   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
   <i class="fas fa-save" aria-hidden="true">save</i>`)
 }
 
-function renderTodos() {
-  todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length;
-
-
-  for (var i = 0; i < todos.length; i++) {
-    var todos = todos[i];
-
-  }
+const btnSave = $(".btn.saveBtn");
+function saveProjectsToStorage(hourData) {
+  localStorage.setItem('hourData', JSON.stringify(hourData));
 }
-
 function plannerFromStorage() {
-  var dayData = localStorage.getItem("hourData");
-  if (dayData) {
-    dayData = JSON.parse(dayData);
+  var Data = localStorage.getItem("hourData");
+  if (Data) {
+    Data = JSON.parse("hourData");
   } else {
-    dayData = [];
+    Data = [];
   }
-  return dayData;
+  return Data;
 }
+btnSave.on("click", saveProjectsToStorage);
 
 // Takes an array of dayData and saves them in localStorage.
-function saveProjectsToStorage(dayData) {
-  localStorage.setItem('dayData', JSON.stringify(dayData));
-}
 
 
 
